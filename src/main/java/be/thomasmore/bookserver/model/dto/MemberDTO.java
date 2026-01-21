a6618328a0c21ab5b434b0053b3128c81b5ae9d7
markj <markjangks@gmail.com>
2026-01-21 05:02:17 +0100

    vraag2: MemberDTO.java

---
+++ src/main/java/be/thomasmore/bookserver/model/dto/MemberDTO.java
@@ -0,0 +1,16 @@
+package be.thomasmore.bookserver.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class MemberDTO {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
