3e3811d44fd17f4cd0303249fd8664bc5c469384
markj <markjangks@gmail.com>
2026-01-21 05:44:58 +0100

    vraag3: controller added_create

--- src/main/java/be/thomasmore/bookserver/controllers/MemberController.java
+++ src/main/java/be/thomasmore/bookserver/controllers/MemberController.java
@@ -5,10 +5,8 @@ import be.thomasmore.bookserver.services.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/members")
@@ -24,5 +22,14 @@ public class MemberController {
     public MemberDetailedDTO findOne(@PathVariable int id) {
         log.info(String.format("##### findOne member %d", id));
         return memberService.findOne(id);
+    }
+
+    @Operation(summary = "create a new member.",
+    description = "Creates a new member in the database and returns the created member.")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberDetailedDTO create(@RequestBody MemberDetailedDTO memberDetailedDTO) {
+        log.info("##### create member");
+        return memberService.create(memberDetailedDTO);
     }
 }
